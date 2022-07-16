63e798b88dc5c29664beda2d5299e7018653c152
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2022-07-16 23:57:17 +0300

    Added ClockHolder

--- src/main/java/com/mfvanek/salary/calc/config/ClockConfig.java
+++ src/main/java/com/mfvanek/salary/calc/config/ClockConfig.java
@@ -3,13 +3,20 @@ package com.mfvanek.salary.calc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Nonnull;
+import javax.validation.ClockProvider;
 import java.time.Clock;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class ClockConfig {
 
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return ClockHolder.getClock();
+    }
+
+    @Bean
+    public ClockProvider clockProvider(@Nonnull final Clock clock) {
+        return () -> clock;
     }
 }
--- src/main/java/com/mfvanek/salary/calc/config/OpenApiConfig.java
+++ src/main/java/com/mfvanek/salary/calc/config/OpenApiConfig.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class OpenApiConfig {
 
     @Bean
