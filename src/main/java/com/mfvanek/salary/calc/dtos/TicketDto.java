24d72a231a7a898e280f907c90ad6baff65850dc
Ivan Vakhrushev <mfvanek@gmail.com>
2023-12-29 10:54:56 +0400

    Add tests

--- src/main/java/com/mfvanek/salary/calc/dtos/TicketDto.java
+++ src/main/java/com/mfvanek/salary/calc/dtos/TicketDto.java
@@ -2,11 +2,15 @@ package com.mfvanek.salary.calc.dtos;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
+@SuperBuilder
+@RequiredArgsConstructor
 public final class TicketDto {
 
     @NotNull
---
+++ src/test/java/com/mfvanek/salary/calc/controllers/TicketControllerTest.java
@@ -0,0 +1,29 @@
+package com.mfvanek.salary.calc.controllers;
+
+import com.mfvanek.salary.calc.dtos.TicketDto;
+import com.mfvanek.salary.calc.support.TestBase;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TicketControllerTest extends TestBase {
+
+    @Test
+    void getTicketShouldReturnNotFoundForUnknownId() {
+        final TicketDto ticket = webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/ticket/{id}")
+                        .build(UUID.randomUUID()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(TicketDto.class)
+                .returnResult()
+                .getResponseBody();
+        assertThat(ticket)
+                .isNotNull()
+                .isEqualTo(TicketDto.builder().build());
+    }
+}
