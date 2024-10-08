5279eb8259d3ae84e1bd23766ce12ccf9c32e19b
Marina Zharinova <106971617+marijarin@users.noreply.github.com>
2024-10-08 23:28:54 +0500

    add jpa query timeout and tests for it (#244)
    * add jpa query timeout and tests for it
    
    * add check exception message, unit for timeout, checkstyle
    
    ---------
    
    Co-authored-by: m.zharinova <m.zharinova@tbank.ru>
    Co-authored-by: Ivan Vakhrushev <mfvanek@gmail.com>

---
+++ src/test/java/com/mfvanek/salary/calc/repositories/JpaPropertiesTest.java
@@ -0,0 +1,37 @@
+package com.mfvanek.salary.calc.repositories;
+
+import com.mfvanek.salary.calc.support.TestBase;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.QueryTimeoutException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JpaPropertiesTest extends TestBase {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    void failsWhenQueryTooLong() {
+        final Query query = entityManager
+                .createNativeQuery(
+                        "SELECT pg_sleep(5)");
+        assertThatThrownBy(query::getResultList)
+                .isInstanceOf(QueryTimeoutException.class)
+                .hasMessageContaining("ERROR: canceling statement due to user request");
+    }
+
+    @Test
+    @DisplayName("Does not throw exception when query does not exceed timeout")
+    void exceptionWithNotLongQuery() {
+        final Query query = entityManager
+                .createNativeQuery(
+                        "SELECT pg_sleep(3.9)");
+        assertThatNoException().isThrownBy(query::getResultList);
+    }
+}
