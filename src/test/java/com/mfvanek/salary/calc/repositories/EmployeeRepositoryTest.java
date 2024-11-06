521f2190400714241043186cd5aab7d81a1c8894
Ivan Vakhrushev <mfvanek@gmail.com>
2024-11-06 20:00:18 +0400

    Add tests

--- src/test/java/com/mfvanek/salary/calc/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/mfvanek/salary/calc/repositories/EmployeeRepositoryTest.java
@@ -5,6 +5,7 @@ import com.mfvanek.salary.calc.support.TestBase;
 import com.mfvanek.salary.calc.support.TestDataProvider;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -20,12 +21,30 @@ class EmployeeRepositoryTest extends TestBase {
         final Employee notSaved = TestDataProvider.prepareIvanIvanov();
         assertThat(notSaved.getCreatedAt())
             .isNull();
+        assertThat(notSaved.getUpdatedAt())
+            .isNull();
         final Employee saved = employeeRepository.save(notSaved);
         assertThat(saved)
             .isNotNull()
             .satisfies(e -> assertThat(e.getCreatedAt())
                 .isEqualTo(ZonedDateTime.of(LocalDateTime.of(1999, Month.DECEMBER, 31, 23, 59, 59), clock.getZone()))
-                .isBefore(ZonedDateTime.now(Clock.systemUTC())));
+                .isBefore(ZonedDateTime.now(Clock.systemUTC())))
+            .satisfies(e -> assertThat(e.getUpdatedAt()).isNull());
+    }
+
+    @Test
+    void createdAtShouldNotBeReplacedWhenExplicitlySet() {
+        final Employee notSaved = TestDataProvider.prepareIvanIvanov();
+        notSaved.setCreatedAt(ZonedDateTime.now(clock).minusYears(1L));
+        assertThat(notSaved.getUpdatedAt())
+            .isNull();
+        final Employee saved = employeeRepository.save(notSaved);
+        assertThat(saved)
+            .isNotNull()
+            .satisfies(e -> assertThat(e.getCreatedAt())
+                .isEqualTo(ZonedDateTime.of(LocalDateTime.of(1998, Month.DECEMBER, 31, 23, 59, 59), clock.getZone()))
+                .isBefore(ZonedDateTime.now(clock)))
+            .satisfies(e -> assertThat(e.getUpdatedAt()).isNull());
     }
 
     @Test
@@ -36,12 +55,29 @@ class EmployeeRepositoryTest extends TestBase {
         final Employee notSaved = TestDataProvider.prepareIvanIvanov();
         assertThat(notSaved.getCreatedAt())
             .isNull();
+        assertThat(notSaved.getUpdatedAt())
+            .isNull();
         final Employee saved = employeeRepository.save(notSaved);
         assertThat(saved)
             .isNotNull()
             .satisfies(e -> assertThat(e.getCreatedAt())
                 .isEqualTo(ZonedDateTime.of(distantFuture, clock.getZone()))
-                .isAfter(ZonedDateTime.now(Clock.systemUTC())));
+                .isAfter(ZonedDateTime.now(Clock.systemUTC())))
+            .satisfies(e -> assertThat(e.getUpdatedAt()).isNull());
+    }
+
+    @Test
+    void updatedAtShouldBeSetAutomaticallyOnUpdate() {
+        final Employee notSaved = TestDataProvider.prepareIvanIvanov();
+        assertThat(notSaved.getUpdatedAt())
+            .isNull();
+        final Employee saved = employeeRepository.save(notSaved);
+        saved.setSalaryPerHour(saved.getSalaryPerHour().multiply(BigDecimal.TEN));
+        final Employee updated = employeeRepository.saveAndFlush(saved);
+        assertThat(updated)
+            .isNotNull()
+            .satisfies(e -> assertThat(e.getUpdatedAt())
+                .isEqualTo(ZonedDateTime.now(clock)));
     }
 
     @Test
