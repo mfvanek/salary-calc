4e334e31760c2660f1e333dbc81c0ebb02f840b1
Ivan Vakhrushev <mfvanek@gmail.com>
2025-04-21 23:00:28 +0400

    Update PostgreSQL version in tests and CI

--- src/main/java/com/mfvanek/salary/calc/config/DatabaseConfig.java
+++ src/main/java/com/mfvanek/salary/calc/config/DatabaseConfig.java
@@ -18,7 +18,7 @@ import javax.sql.DataSource;
 @Configuration(proxyBeanMethods = false)
 public class DatabaseConfig {
 
-    public static final String PG_IMAGE = "postgres:17.0";
+    public static final String PG_IMAGE = "postgres:17.4";
 
     @SuppressWarnings({"resource", "java:S2095", "java:S1452"})
     @Bean(initMethod = "start", destroyMethod = "stop")
